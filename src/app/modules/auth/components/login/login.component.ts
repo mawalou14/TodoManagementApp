a62f24eb95253586389db5a28aa16e6abc4fd1bb
mawalou14 <awaloum213@gmail.com>
2024-07-10 11:34:20 +0100

    Style auth components

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  passwordFieldTypeText: boolean = false;
 
+  togglePasswordFieldType() {
+    this.passwordFieldTypeText = !this.passwordFieldTypeText;
+  }
 }
--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  passwordFieldTypeText: boolean = false;
+  confirmPasswordFieldTypeText: boolean = false;
 
+  togglePasswordFieldType() {
+    this.passwordFieldTypeText = !this.passwordFieldTypeText;
+  }
+  toggleConfirmPasswordFieldType() {
+    this.confirmPasswordFieldTypeText = !this.confirmPasswordFieldTypeText;
+  }
 }
