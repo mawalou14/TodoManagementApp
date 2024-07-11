475e0bc9d939909eea5227b62aafadcd56433ae4
mawalou14 <awaloum213@gmail.com>
2024-07-11 12:45:56 +0100

    add auth service methods

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { LoginForm } from '../models/login';
+import { AuthResponse } from '../models/authResponse';
+import { Observable, tap } from 'rxjs';
+import { RegisterForm } from '../models/register';
 
 @Injectable({
   providedIn: 'root',
@@ -7,4 +11,41 @@ import { inject, Injectable } from '@angular/core';
 export class AuthService {
   private httpClient = inject(HttpClient);
   private url = 'auth';
+
+  login(loginForm: LoginForm): Observable<AuthResponse> {
+    return this.httpClient
+      .post<AuthResponse>(`${this.url}/login`, loginForm)
+      .pipe(
+        tap((response) => {
+          localStorage.setItem('user', JSON.stringify(response));
+        })
+      );
+  }
+
+  register(registerForm: RegisterForm): Observable<AuthResponse> {
+    return this.httpClient
+      .post<AuthResponse>(`${this.url}/register`, registerForm)
+      .pipe(
+        tap((response) => {
+          localStorage.setItem('user', JSON.stringify(response));
+        })
+      );
+  }
+
+  getToken(): string | null {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      const user: AuthResponse = JSON.parse(storedUser);
+      return user.token;
+    }
+    return null;
+  }
+
+  logout(): void {
+    localStorage.removeItem('user');
+  }
+
+  get isLoggedIn(): boolean {
+    return localStorage.getItem('user') !== null;
+  }
 }
