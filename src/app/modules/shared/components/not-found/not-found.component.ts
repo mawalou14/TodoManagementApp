72abcf1f9e92aec01264fbd69dc1a309c941d92e
mawalou14 <awaloum213@gmail.com>
2024-07-20 10:35:07 +0100

    update modal size

--- src/app/modules/shared/components/not-found/not-found.component.ts
+++ src/app/modules/shared/components/not-found/not-found.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
+import { LoadinServiceService } from '../../services/loadingService/loadin.service.service';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
 })
 export class NotFoundComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
+  onMoveToAuthModule(route: string): void {
+    // this.loadingService.setLoading(true);
+    setTimeout(() => {
+      // this.loadingService.setLoading(false);
+      this.authService.logout();
+      this.router.navigate([`/${route}`]);
+    }, 1000);
+  }
 }
